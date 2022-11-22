88a65e13854872e93247498fe48edfce01f949c7
SimonGideon <simongideon918@gmail.com>
2022-11-23 02:43:00 +0300

    Home page setup

---
+++ src/modules/show_meals.js
@@ -0,0 +1,24 @@
+const show = (arr) => {
+  let html = '';
+  // eslint-disable-next-line no-restricted-syntax
+  for (const meal of arr) {
+    html += `
+    <div class="individuals">
+          <img src="${meal.strCategoryThumb}" alt="foodpad">
+          <div>
+            ${meal.strCategory} ${meal.idCategory}
+            <div>
+              <i class="fa-solid fa-heart"></i>
+              5 likes
+            </div>
+          </div>
+          <button id="comment">Comments</button>
+          <button id="Reservations">Reservations</button>
+     </div>
+        `;
+  }
+
+  const mainContent = document.getElementById('meals-content');
+  mainContent.innerHTML = html;
+};
+export default show;
