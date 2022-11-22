cb3d9de61cd1f8851baf6ef42f7e8f4cd62d9e7e
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-11-22 07:40:03 -0800

    assets added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,2 @@
-import components from './components.js';
-// image
-import logo from './images/icons8-disney-logo.svg';
 // styles
 import './styles/styles.css';
-
-const img = document.createElement('img');
-img.src = logo;
-document.body.appendChild(img);
-document.body.appendChild(components());
--- src/components.js
+++
@@ -1,7 +0,0 @@
-const components = () => {
-  const element = document.createElement('h2');
-  element.innerHTML = 'Welcome to this site';
-  return element;
-};
-
-export default components;
